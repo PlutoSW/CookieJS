07b894bea1be256a8e8bc1ad47fcfcd1c961c923
PlutoSW <31663648+PlutoSW@users.noreply.github.com>
2020-02-26 06:36:24 +0300

    Create cookie.js

---
+++ cookie.js
@@ -0,0 +1,78 @@
+class Cookie {
+    constructor() {
+        this.cookies = [];
+        this.all();
+    }
+    parse(c) {
+        var arr = c.split('='),
+            key = arr[0],
+            value = null;
+        if (arr.length > 1) {
+            value = arr.slice(1).join('');
+        }
+        return {
+            key,
+            value
+        }
+    }
+    all() {
+        document.cookie.split(';')
+            .map(m => {
+                return m.replace(/^\s+/, '').replace(/\s+$/, '');
+            })
+            .forEach(c => {
+                var p = this.parse(c);
+                if (p.value != null) {
+                    var key = p.key;
+                    Object.defineProperty(this, key, {
+                        get() {
+                            this.value = this.get(key).value;
+                            if (this.value || this.value !== "null") {
+                                this.key = key;
+                                return this;
+                            } else {
+                                return null;
+                            }
+                        }
+                    });
+                    this.cookies.push({
+                        key: p.key,
+                        value: p.value
+                    });
+                }
+            });
+        return this.cookies;
+    }
+    get(key) {
+        var result;
+        if (key.constructor == RegExp) {
+            result = this.cookies.find(e => key.test(e.key));
+        } else {
+            result = this.cookies.find(e => e.key === key);
+        }
+        return result;
+    }
+    remove() {
+        this.set(this.key, null, -1);
+    }
+    set(key, value, time) {
+        var today = new Date(),
+            offset = (typeof time == 'undefined') ? (1000 * 60 * 60 * 24) : (time * 1000),
+            expires_at = new Date(today.getTime() + offset);
+        document.cookie = key + "=" + value + ";" + expires_at + ";path=/";
+        if (!this.get("abc")) {
+            Object.defineProperty(this, key, {
+                get() {
+                    this.key = key;
+                    return this;
+                }
+            });
+            this.cookies.push({
+                key: key,
+                value: value
+            });
+        } else {
+            this.get(key).value = value;
+        }
+    }
+}
